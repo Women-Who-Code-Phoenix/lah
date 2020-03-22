99b51df357b48083d3954060ec4b5dcd7838615e
rvehall <raneith.dev@gmail.com>
2020-03-22 01:23:26 -0700

    Base app

---
+++ src/components/Navigation.vue
@@ -0,0 +1,76 @@
+<template>
+  <span>
+    <v-app-bar
+      color="#fafafa"
+      :collapse="!collapseOnScroll"
+      :collapse-on-scroll="collapseOnScroll"
+      fixed
+      app
+      :clipped-left="clipped"
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Life From Home</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-checkbox v-model="collapseOnScroll" color="primary" hide-details></v-checkbox>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="clipped"
+      enable-resize-watcher
+      app
+      dark
+      class="primary"
+    >
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="white--text text--accent-4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </span>
+</template>
+
+<script>
+// import * as firebase from "firebase/app";
+
+export default {
+  data: () => ({
+    drawer: true,
+    collapseOnScroll: true,
+    clipped: false,
+    group: null,
+    firestore: firebase.firestore()
+  }),
+  mounted() {
+    this.fetchData();
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    }
+  },
+  methods: {
+    fetchData() {
+    //   this.firestore.getCollections().then(collections => {
+    //     for (let collection of collections) {
+    //       console.log(`Found collection with id: ${collection.id}`);
+    //     }
+    //   });
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import * as VueFire from 'vuefire';
+import firebase from 'firebase';
+
+Vue.use(VueFire);
+
+const config = {
+  apiKey: "{key}",
+  authDomain: "funfromhome-{id}.firebaseapp.com",
+  databaseURL: "https://funfromhome-{id}.firebaseio.com",
+  projectId: "funfromhome-{id}",
+  storageBucket: "funfromhome-{id}.appspot."
+}
+
+firebase.initializeApp(config);
+
+// });
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
