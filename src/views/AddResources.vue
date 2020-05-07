39c6a1f28df779e320fcc88c2b2ad441cb80b680
Ana Tomboulian <ana.tomboulian@attainia.com>
2020-05-06 19:16:01 -0700

    resource add

---
+++ src/views/AddResources.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from '@/components/Form.vue'
+
+export default {
+  name: 'AddResources',
+  components: {
+    Form,
+  },
+}
+</script>
